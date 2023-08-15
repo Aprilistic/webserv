d92860769dc7f5b0c47234960e37acb3eec35eab
Jinho Heo <uj07077@gmail.com>
2023-08-15 10:28:30 +0900

    Remove: Multiprocess, multithread structure deprecated

--- srcs/core/NgxProcess.cpp
+++
@@ -1,6 +0,0 @@
-#include "NgxProcess.hpp"
-
-pid_t NgxProcess::NgxFork(int (*fn)(void *), void *childStack, int flags,
-                          void *data) {
-  return (clone(fn, childStack, flags, data));
-}
