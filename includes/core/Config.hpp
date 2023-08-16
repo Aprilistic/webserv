13595260fbd5ee38dd5da4b6ca4cac217ea189a0
Jinho Heo <uj07077@gmail.com>
2023-08-16 13:52:25 +0900

    Feat: Config struct recursive

--- includes/core/Config.hpp
+++ includes/core/Config.hpp
@@ -38,40 +38,5 @@ private:
   std::string mServerName;
   std::map<int, std::string> mReturn;
   std::vector<std::string> mLimitExcept;
-  limit_except GET POST
-  
 };
-
-#endif
-
-    /*
-    #default conf
-
-    error_page(http, server, location) 404 500 /path/error.html;
-    client_max_body_size(http, server, location) 1M;
-    alias(http, server, location)
-    autoindex(http, server, location)
-    index(http, server, location)
-
-    server {
-            listen(server) 8080 default_server;
-            server_name(server) example.com;
-            return(server, location)
-
-            location {
-                    limit_except(location) GET POST {
-                                    deny all;
-                    }
-            }
-            
-    }
-    //
-    server {
-            listen
-            server_name
-            return
-    }
-
-
-    */
 #endif
