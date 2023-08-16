9ca9c5f09894a8ae095a0528310e55a8463f47aa
Jinho Heo <uj07077@gmail.com>
2023-08-16 13:39:37 +0900

    Feat: New structure for config parsing ```

--- includes/core/Config.hpp
+++ includes/core/Config.hpp
@@ -20,30 +20,58 @@ private:
   std::stringstream mConfBuffer;
 };
 
-class CoreModule {
+class ServerInfo {
 public:
-protected:
-  int mTimeOut; /* Round-Robin time slice */
-  int mClientMaxBodySize;
-  std::string mUploadStore;
-};
+  void ParseConfig(std::string &fileName);
+private:
+  void setServer(int level, ServerInfo parentData);
+  void setLocation(int level, LocationInfo parentData);
 
-class ServerContext : public CoreModule
-{
 public:
 private:
-  int mListen; // int mPort; std::string mHost;
+  std::map<int, std::string> mErrorPage;
+  int mClientMaxBodySize;
+  std::string mAlias;
+  bool mbAutoIndex;
+  std::string mIndex;
+  std::map<int, std::string> mListen;
   std::string mServerName;
-
-  // location context
-  std::string mRoot;
-  std::vector<std::string> mIndex;
-  std::vector<int> mErrorPageNum;
-  std::string mErrorPage;
-  std::map<std::string, std::map<std::string, std::string> > mLocations; /* <location_dir, <key, filename>> */
-
-  /* ServerContext */
-  /* LocationContext */
+  std::map<int, std::string> mReturn;
+  std::vector<std::string> mLimitExcept;
+  limit_except GET POST
+  
 };
 
+#endif
+
+    /*
+    #default conf
+
+    error_page(http, server, location) 404 500 /path/error.html;
+    client_max_body_size(http, server, location) 1M;
+    alias(http, server, location)
+    autoindex(http, server, location)
+    index(http, server, location)
+
+    server {
+            listen(server) 8080 default_server;
+            server_name(server) example.com;
+            return(server, location)
+
+            location {
+                    limit_except(location) GET POST {
+                                    deny all;
+                    }
+            }
+            
+    }
+    //
+    server {
+            listen
+            server_name
+            return
+    }
+
+
+    */
 #endif
