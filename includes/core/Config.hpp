b8fe6d84f9c60ddaff95361be05f8bf7e1f703ba
Hyeon Jun An <hyeoan@c4r5s1.42seoul.kr>
2023-08-17 16:52:56 +0900

    Test : parsing test

--- includes/core/Config.hpp
+++ includes/core/Config.hpp
@@ -1,30 +1,43 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-#include "Core.hpp"
+#include "/Users/hyeoan/goinfre/web/includes/core/Core.hpp"
 
+// class ServerBlock;
+// class LocationBlock;
 class Config {
   /* methods */
 public:
-  Config();
+  Config(const std::string &path);
+void SetConfig(const std::string &path);
 
-  const int GetWorkerCount() const;
-  const int GetWorkerConnectionCount() const;
-
-protected:
 private:
+void openConfFile(const std::string &path);
+void parseConfFile(void);
+void removeComment(void);
+void addBlank(void);
+void tokenize(void);
   /* variables */
-protected:
-  /* MainContext */
-  int mWorkerCount;
-
-  /* EventsContext */
-  int mWorkerConnectionCount;
-
-  /* HttpContext */
-
-  /* ServerContext */
-  /* LocationContext */
+private:
+  std::stringstream mConfBuffer;
+//   std::map<std::string, ServerBlock> mServers;
+  std::vector<std::string> mTokens;
 };
+
+// class ServerBlock {
+// public:
+// private:
+//   LocationBlock *mRoot;
+//   std::map<std::string, LocationBlock *> mLocation; /* URI 경로, 노드 주소 */
+// };
+
+// class LocationBlock {
+// public:
+// private:
+//   std::string mFragment; /* URI */
+//   std::map<std::string, std::vector<std::string> > mDirectives;
+//   LocationBlock *mParent; /* NULL이면 rootNode */
+//   std::map<std::string, LocationBlock *> mChildren;
+// };
 
 #endif
--- srcs/core/Config.cpp
+++ srcs/core/Config.cpp
@@ -1,9 +1,100 @@
-#include "Config.hpp"
+#include "/Users/hyeoan/goinfre/web/includes/core/Config.hpp"
 
-Config::Config() : mWorkerCount(4), mWorkerConnectionCount(1024) {}
-
-const int Config::GetWorkerCount() const { return mWorkerCount; }
-
-const int Config::GetWorkerConnectionCount() const {
-  return mWorkerConnectionCount;
+Config::Config(const std::string &path) {
+  SetConfig(path);
 }
+
+void Config::SetConfig(const std::string &path) {
+  openConfFile(path);
+  removeComment();
+  addBlank();
+  tokenize();
+  parseConfFile();
+}
+
+void Config::openConfFile(const std::string &path) {
+  std::ifstream confFile(path);
+  if (confFile.is_open() == false) {
+    std::cerr << "Error: Could not open confFile " << path << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  mConfBuffer << confFile.rdbuf();
+  confFile.close();
+  if (mConfBuffer.good() == false) {
+    std::cerr << "Error: confFile stream " << path << std::endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
+void Config::removeComment(void)
+{
+	std::stringstream cleanBuffer;
+	std::string line;
+	int	commentPos;
+
+	while (std::getline(mConfBuffer, line))
+	{
+		commentPos = line.find('#');
+		if (commentPos != std::string::npos)
+			line.erase(commentPos);
+		cleanBuffer << line << std::endl;
+	}
+    mConfBuffer.str(cleanBuffer.str());
+    mConfBuffer.clear();
+
+}
+
+void Config::addBlank(void)
+{
+	std::string content;
+	bool insideQuote = false;
+
+	content = mConfBuffer.str();
+	for (int index = 0; index < content.size(); index++)
+	{
+		if (content[index] == '"')
+		{
+			insideQuote = !insideQuote;
+		}
+		if (insideQuote == false)
+		{
+			if (content[index] == '{' ||
+				content[index] == '}' ||
+				content[index] == ';')
+			{
+				content.insert(index, " ");
+				index += 2;
+				content.insert(index, " ");
+			}
+		}
+	}
+
+	mConfBuffer.str("");
+	mConfBuffer << content;
+}
+
+void Config::tokenize(void)
+{
+	std::string token;
+
+	while (mConfBuffer.eof() == false)
+	{
+		mConfBuffer >> token;
+		printf("token: %s\n", token.c_str());
+		mTokens.push_back(token);
+	}
+}
+
+// void Config::checkSyntax(void)
+// {}
+
+void Config::parseConfFile(void)
+{
+	for (std::vector<std::string>::iterator it = mTokens.begin(); it != mTokens.end(); ++it)
+	{
+		std::cout << *it << std::endl;
+	}
+}
+
+
