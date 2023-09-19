5d0ca6c54375219e6a19cf6baba6fe20fa23ddfd
euiclee <lechan1317@gmail.com>
2023-09-20 08:47:00 +0900

    Refactor: formatter

--- srcs/core/NodeCheckSum.cpp
+++ srcs/core/NodeCheckSum.cpp
@@ -1,8 +1,7 @@
 #include "Node.hpp"
 
-void Node::checkSum(int level)
-{
-  for (std::map<std::string, std::vector<std::string> >::iterator it =
+void Node::checkSum(int level) {
+  for (std::map<std::string, std::vector<std::string>>::iterator it =
            mDirectives.begin();
        it != mDirectives.end(); ++it) {
     if (it->first == "error_page")
@@ -27,12 +26,13 @@ void Node::checkSum(int level)
   for (std::vector<Node *>::iterator it = mChildren.begin();
        it != mChildren.end(); ++it) {
     (*it)->checkSum(level + 1);
-       }
+  }
 }
 
 void Node::CheckErrorPage(std::vector<std::string> &value) {
   if (value.size() < 2) {
-    nodeError("Error: Incorrect number of arguments. At least two arguments are required.");
+    nodeError("Error: Incorrect number of arguments. At least two arguments "
+              "are required.");
   }
   for (std::vector<std::string>::iterator it = value.begin();
        it != value.end() - 1; it++) {
@@ -52,7 +52,8 @@ void Node::CheckErrorPage(std::vector<std::string> &value) {
 
 void Node::CheckClientMaxBodySize(std::vector<std::string> &value) {
   if (value.size() != 1) {
-    nodeError("Error: Incorrect number of arguments. Exactly one argument is required.");
+    nodeError("Error: Incorrect number of arguments. Exactly one argument is "
+              "required.");
   }
   int multiplier;
   char lastChar = value[0].back();
@@ -97,14 +98,16 @@ void Node::CheckClientMaxBodySize(std::vector<std::string> &value) {
 
 void Node::CheckIndex(std::vector<std::string> &value) {
   if (value.size() < 1) {
-    nodeError("Error: No arguments provided. At least one argument is required.");
+    nodeError(
+        "Error: No arguments provided. At least one argument is required.");
   }
 }
 
 void Node::CheckAutoIndex(std::vector<std::string> &value) {
 
   if (value.size() != 1) {
-    nodeError("Error: Incorrect number of arguments. Exactly one argument is required.");
+    nodeError("Error: Incorrect number of arguments. Exactly one argument is "
+              "required.");
   }
   std::string upperName = value[0];
   std::transform(upperName.begin(), upperName.end(), upperName.begin(),
@@ -114,151 +117,125 @@ void Node::CheckAutoIndex(std::vector<std::string> &value) {
   }
 }
 
-void Node::CheckListen(std::vector<std::string> &value)
-{
-	if (value.size() < 1)
-	{
-    	nodeError("Error: No arguments provided. At least one argument is required.");
-	}
-
-	size_t pos = value[0].find(':');
-	// only IP or only Port
-
-	if (pos == std::string::npos)
-	{
-		if(value[0].find('.') != std::string::npos)
-		{
-			// Only IP
-			std::istringstream iss(value[0]);
-			int num;
-			char dot;
-			for (int i = 0; i < 3; i++)
-			{
-				iss >> num >> dot;
-				if (num < 0 || 255 < num || dot != '.')
-				{
-         			nodeError("Error: 0 ~ 255 & only .");
-				}
-			}
-			iss >> num;
-			if (num < 0 || 255 < num || iss.eof() == false)
-			{
-        		nodeError("Error: 0 ~ 255 & only .");
-			}
-		}
-		else
-		{
-			// Only Port
-			for (int i = 0; i < value[0].size(); i++)
-			{
-				if (!isdigit(value[0][i]))
-				{
-          			nodeError("Error: Invalid port number.");
-				}
-			}
-			int port = atoi(value[0].c_str());
-			if (port < 0 || 65535 < port)
-			{
-        		nodeError("Error: 0 ~ 65535");
-			}
-		}
-	}
-	else
-	{
-		// IP:Port 127.0.0.1:8080
-		std::string ip = value[0].substr(0, pos); // ex 127.0.0.1
-		std::string portStr = value[0].substr(pos + 1); // 8080
-		int port = atoi(portStr.c_str()); //8080
-
-		// IP validate
-		int num;
-		char dot;
-		std::istringstream iss(ip);
-
-		for (int i = 0; i < 3; i++)
-		{
-			iss >> num >> dot;
-			if (num < 0 || 255 < num || dot != '.')
-			{
-        		nodeError("Error: 0 ~ 255 & only .");
-			}
-		}
-		iss >> num;
-		if (num < 0 || 255 < num || iss.eof() == false)
-		{
-      		nodeError("Error: 0 ~ 255 & only .");
-		}
-
-		// Port validate
-		for(std::string::size_type i = 0; i < portStr.size(); ++i)
-		{
-			if(!isdigit(portStr[i]))
-			{
-        		nodeError("Error: Invalid port number.");
-			}
-		}
-
-		if (port < 0 || 65535 < port)
-		{
-     		nodeError("Error: 0 ~ 65535");
-		}
-	}
-}
-
-void Node::CheckServerName(std::vector<std::string> &value)
-{
+void Node::CheckListen(std::vector<std::string> &value) {
   if (value.size() < 1) {
-    nodeError("Error: No arguments provided. At least one argument is required.");
+    nodeError(
+        "Error: No arguments provided. At least one argument is required.");
+  }
+
+  size_t pos = value[0].find(':');
+  // only IP or only Port
+
+  if (pos == std::string::npos) {
+    if (value[0].find('.') != std::string::npos) {
+      // Only IP
+      std::istringstream iss(value[0]);
+      int num;
+      char dot;
+      for (int i = 0; i < 3; i++) {
+        iss >> num >> dot;
+        if (num < 0 || 255 < num || dot != '.') {
+          nodeError("Error: 0 ~ 255 & only .");
+        }
+      }
+      iss >> num;
+      if (num < 0 || 255 < num || iss.eof() == false) {
+        nodeError("Error: 0 ~ 255 & only .");
+      }
+    } else {
+      // Only Port
+      for (int i = 0; i < value[0].size(); i++) {
+        if (!isdigit(value[0][i])) {
+          nodeError("Error: Invalid port number.");
+        }
+      }
+      int port = atoi(value[0].c_str());
+      if (port < 0 || 65535 < port) {
+        nodeError("Error: 0 ~ 65535");
+      }
+    }
+  } else {
+    // IP:Port 127.0.0.1:8080
+    std::string ip = value[0].substr(0, pos);       // ex 127.0.0.1
+    std::string portStr = value[0].substr(pos + 1); // 8080
+    int port = atoi(portStr.c_str());               // 8080
+
+    // IP validate
+    int num;
+    char dot;
+    std::istringstream iss(ip);
+
+    for (int i = 0; i < 3; i++) {
+      iss >> num >> dot;
+      if (num < 0 || 255 < num || dot != '.') {
+        nodeError("Error: 0 ~ 255 & only .");
+      }
+    }
+    iss >> num;
+    if (num < 0 || 255 < num || iss.eof() == false) {
+      nodeError("Error: 0 ~ 255 & only .");
+    }
+
+    // Port validate
+    for (std::string::size_type i = 0; i < portStr.size(); ++i) {
+      if (!isdigit(portStr[i])) {
+        nodeError("Error: Invalid port number.");
+      }
+    }
+
+    if (port < 0 || 65535 < port) {
+      nodeError("Error: 0 ~ 65535");
+    }
   }
 }
 
-
-void Node::CheckReturn(std::vector<std::string> &value)
-{
-	if (value.size() != 2)
-	{
-    nodeError("Error: Incorrect number of arguments. Exactly two arguments are required.");  
-	}
-    std::string firstValue = value[0];
-
-    for (int i = 0; i < firstValue.size(); i++)
-    {
-        if (isdigit(firstValue[i]) == false)
-        {
-          nodeError("Error: Invalid character found in status code argument.");
-        }
-    }
-
-	int num = atoi(firstValue.c_str());
-	if (num < 0 || num > 999)
-	{
-		nodeError("Error: The first value must be a number within the range 0-999.");
-	}
-}
-
-void Node::CheckAlias(std::vector<std::string> &value)
-{
-	if (value.size() != 1) {
-    	nodeError("Error: No arguments provided. At least one argument is required.");
+void Node::CheckServerName(std::vector<std::string> &value) {
+  if (value.size() < 1) {
+    nodeError(
+        "Error: No arguments provided. At least one argument is required.");
   }
 }
 
-void Node::CheckLimitExcept(std::vector<std::string> &value)
-{
-	if (value.size() < 1)
-	{
-		nodeError("Error:");
-	}
-	std::vector<std::string> validMethods;
-    validMethods.push_back("GET");
-    validMethods.push_back("POST");
-    validMethods.push_back("DELETE");
+void Node::CheckReturn(std::vector<std::string> &value) {
+  if (value.size() != 2) {
+    nodeError("Error: Incorrect number of arguments. Exactly two arguments are "
+              "required.");
+  }
+  std::string firstValue = value[0];
 
-    for(int i = 0; i < value.size(); i++)
-    {
-        if(std::find(validMethods.begin(), validMethods.end(), value[i]) == validMethods.end())
-        {
-            nodeError("Invalid method");
-        }
+  for (int i = 0; i < firstValue.size(); i++) {
+    if (isdigit(firstValue[i]) == false) {
+      nodeError("Error: Invalid character found in status code argument.");
     }
+  }
+
+  int num = atoi(firstValue.c_str());
+  if (num < 0 || num > 999) {
+    nodeError(
+        "Error: The first value must be a number within the range 0-999.");
+  }
+}
+
+void Node::CheckAlias(std::vector<std::string> &value) {
+  if (value.size() != 1) {
+    nodeError(
+        "Error: No arguments provided. At least one argument is required.");
+  }
+}
+
+void Node::CheckLimitExcept(std::vector<std::string> &value) {
+  if (value.size() < 1) {
+    nodeError("Error:");
+  }
+  std::vector<std::string> validMethods;
+  validMethods.push_back("GET");
+  validMethods.push_back("POST");
+  validMethods.push_back("DELETE");
+
+  for (int i = 0; i < value.size(); i++) {
+    if (std::find(validMethods.begin(), validMethods.end(), value[i]) ==
+        validMethods.end()) {
+      nodeError("Invalid method");
+    }
+  }
 }
