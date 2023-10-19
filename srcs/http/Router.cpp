21555872cb98ebda42d6fdc5a084cd045c814bdc
euiclee <lechan1317@gmail.com>
2023-10-19 10:37:28 +0900

    Feat: deleteHandler add

--- srcs/http/Router.cpp
+++ srcs/http/Router.cpp
@@ -214,13 +214,59 @@ eStatusCode PostHandler::handle(int port, Http &http) {
 }
 
 eStatusCode DeleteHandler::handle(int port, Http &http) {
-  eStatusCode res;
-  http.getResponse().mStatusCode = 200;
+  // URI로 리소스 위치 확인
+  Node *location = Common::mConfigMap->GetConfigNode(
+      port, http.getRequest().mHost, http.getRequest().mUri);
+  if (location == NULL) {
+    http.getResponse().mStatusCode = CLIENT_ERROR_NOT_FOUND;
+    return CLIENT_ERROR_NOT_FOUND;
+  }
 
-  http.getResponse().mBody =
-      "DELETE request received for URI: " + http.getRequest().mUri;
+  // location 정보에서 alias, uri 정보 가져오기
+  std::vector<std::string> uri = location->FindValue(location, "location");
+  std::vector<std::string> alias = location->FindValue(location, "alias");
 
-  return res;
+  // file path 구하기
+  std::string resolvedPath;
+  if (alias.empty()) {
+    if (uri[0] == "/") {
+      // 어떤 동작하는지 찾아봐야 함
+      http.getResponse().mStatusCode = CLIENT_ERROR_NOT_FOUND;
+      return CLIENT_ERROR_NOT_FOUND;
+    }
+  } else {
+    resolvedPath = uri[0] + alias[0];
+  }
+
+  // 데이터 삭제
+  switch (http.CheckPathType(resolvedPath)) {
+  case PATH_IS_DIRECTORY: {
+    //DELETE 요청은 리소스와 관련된 메타데이터를 모두 삭제해야 함
+    //하지만 디렉토리를 삭제하는 것은 위험할 수 있어서 허용하지 않을 수 있음
+    http.getResponse().mStatusCode = CLIENT_ERROR_METHOD_NOT_ALLOWED;
+    return CLIENT_ERROR_METHOD_NOT_ALLOWED;
+  }
+  case PATH_IS_FILE: {
+    //파일 삭제 로직
+    //삭제에 성공하면 200 OK 또는 204 No Content 반환
+    //실패하면 500 Internal Server Error 반환
+    if (/*파일 삭제 성공*/ true) {
+      http.getResponse().mStatusCode = SUCCESSFUL_OK;
+      return SUCCESSFUL_OK;
+    } else {
+      http.getResponse().mStatusCode = SERVER_ERROR_INTERNAL_SERVER_ERROR;
+      return SERVER_ERROR_INTERNAL_SERVER_ERROR;
+    }
+  }
+  case PATH_INACCESSIBLE: {
+    http.getResponse().mStatusCode = CLIENT_ERROR_FORBIDDEN;
+    return CLIENT_ERROR_FORBIDDEN;
+  }
+  default: {
+    http.getResponse().mStatusCode = CLIENT_ERROR_NOT_FOUND;
+    return CLIENT_ERROR_NOT_FOUND;
+  }
+  }
 }
 
 eStatusCode PutHandler::handle(int port, Http &http) {
