73a3f06146ffcc1e3b89a221b7ad27d7e6b85b6c
euiclee <lechan1317@gmail.com>
2023-10-31 11:16:08 +0900

    Feat: shared_ptr

---
+++ includes/Shared_ptr.hpp
@@ -0,0 +1,63 @@
+#ifndef HTTP_SHARED_PTR_HPP
+#define HTTP_SHARED_PTR_HPP
+
+#include "Core.hpp"
+
+template <typename T> class SharedPtr {
+private:
+  T *ptr;
+  int *refCount;
+
+public:
+  SharedPtr() : ptr(NULL), refCount(new int(0)) {}
+
+  SharedPtr(T *p) : ptr(p), refCount(new int(1)) {}
+
+  SharedPtr(const SharedPtr &other) : ptr(other.ptr), refCount(other.refCount) {
+    (*refCount)++;
+  }
+
+  ~SharedPtr() {
+    if (--(*refCount) == 0) {
+      delete ptr;
+      delete refCount;
+    }
+  }
+
+  SharedPtr &operator=(const SharedPtr &other) {
+    if (this != &other) {
+      if (--(*refCount) == 0) {
+        delete ptr;
+        delete refCount;
+      }
+      ptr = other.ptr;
+      refCount = other.refCount;
+      (*refCount)++;
+    }
+    return *this;
+  }
+
+  T &operator*() const { return *ptr; }
+
+  T *operator->() const { return ptr; }
+
+  void reset() {
+    if (--(*refCount) == 0) {
+      delete ptr;
+      delete refCount;
+    }
+    ptr = NULL;
+    refCount = new int(0);
+  }
+
+  void reset(T *p) {
+    if (--(*refCount) == 0) {
+      delete ptr;
+      delete refCount;
+    }
+    ptr = p;
+    refCount = new int(1);
+  }
+};
+
+#endif
