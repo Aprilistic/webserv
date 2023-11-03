7e9b7daf23ce2e145a239f51483dbd4e87851b68
euiclee <lechan1317@gmail.com>
2023-11-03 09:28:20 +0900

    Fix: formatter make problem

--- includes/core/config/Node.hpp
+++ includes/core/config/Node.hpp
@@ -24,7 +24,7 @@ private:
 
   // Do we really need to make these private?
 public:
-  std::map<std::string, std::vector<std::string>> mDirectives;
+  std::map<std::string, std::vector<std::string> > mDirectives;
   std::vector<Node *> mChildren;
 
 private:
