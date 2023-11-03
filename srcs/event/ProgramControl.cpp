e3f8ccaacc8f46fc083aeed0d886022ae8763617
euiclee <lechan1317@gmail.com>
2023-11-03 09:34:28 +0900

    Style: formatter of programcontrol.cpp

--- srcs/event/ProgramControl.cpp
+++ srcs/event/ProgramControl.cpp
@@ -6,50 +6,50 @@ void ProgramControl::EventHandler(struct kevent &currentEvent) {
   }
   switch (currentEvent.filter) {
   case EVFILT_READ:
-		assert("ProgramControl::EventHandler: EVFILT_READ" == 0);
+    assert("ProgramControl::EventHandler: EVFILT_READ" == 0);
     break;
   case EVFILT_WRITE:
-		assert("ProgramControl::EventHandler: EVFILT_WRITE" == 0);
+    assert("ProgramControl::EventHandler: EVFILT_WRITE" == 0);
     break;
   case EVFILT_TIMER:
-		assert("ProgramControl::EventHandler: EVFILT_TIMER" == 0);
+    assert("ProgramControl::EventHandler: EVFILT_TIMER" == 0);
     break;
   case EVFILT_SIGNAL:
-		signalHandler(currentEvent.ident);
+    signalHandler(currentEvent.ident);
     break;
   default:
-		assert("ProgramControl::EventHandler: default" == 0);
+    assert("ProgramControl::EventHandler: default" == 0);
     break;
   }
 }
 
 void ProgramControl::RegisterTerminationSignals(void) {
-	struct kevent event[3];
+  struct kevent event[3];
 
-	EV_SET(&event[0], SIGINT, EVFILT_SIGNAL, EV_ADD, 0, 0, this);
-	EV_SET(&event[1], SIGTERM, EVFILT_SIGNAL, EV_ADD, 0, 0, this);
-	EV_SET(&event[2], SIGQUIT, EVFILT_SIGNAL, EV_ADD, 0, 0, this);
-	kevent(Common::mKqueue, event, 3, NULL, 0, NULL);
+  EV_SET(&event[0], SIGINT, EVFILT_SIGNAL, EV_ADD, 0, 0, this);
+  EV_SET(&event[1], SIGTERM, EVFILT_SIGNAL, EV_ADD, 0, 0, this);
+  EV_SET(&event[2], SIGQUIT, EVFILT_SIGNAL, EV_ADD, 0, 0, this);
+  kevent(Common::mKqueue, event, 3, NULL, 0, NULL);
 }
 
 void ProgramControl::UnregisterTerminationSignals(void) {
-	struct kevent event[3];
+  struct kevent event[3];
 
-	EV_SET(&event[0], SIGINT, EVFILT_SIGNAL, EV_DELETE, 0, 0, this);
-	EV_SET(&event[1], SIGTERM, EVFILT_SIGNAL, EV_DELETE, 0, 0, this);
-	EV_SET(&event[2], SIGQUIT, EVFILT_SIGNAL, EV_DELETE, 0, 0, this);
-	kevent(Common::mKqueue, event, 3, NULL, 0, NULL);
+  EV_SET(&event[0], SIGINT, EVFILT_SIGNAL, EV_DELETE, 0, 0, this);
+  EV_SET(&event[1], SIGTERM, EVFILT_SIGNAL, EV_DELETE, 0, 0, this);
+  EV_SET(&event[2], SIGQUIT, EVFILT_SIGNAL, EV_DELETE, 0, 0, this);
+  kevent(Common::mKqueue, event, 3, NULL, 0, NULL);
 }
 
 void ProgramControl::signalHandler(int signal) {
-	switch (signal) {
-	case SIGINT:
-	case SIGTERM:
-	case SIGQUIT:
-		Common::mRunning = false;
-		break;
-	default:
-		assert("ProgramControl::signalHandler: default" == 0);
-		break;
-	}
+  switch (signal) {
+  case SIGINT:
+  case SIGTERM:
+  case SIGQUIT:
+    Common::mRunning = false;
+    break;
+  default:
+    assert("ProgramControl::signalHandler: default" == 0);
+    break;
+  }
 }
