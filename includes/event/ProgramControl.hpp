8e9b0d306ac1f419d1087de281ee7edffb8c4c20
euiclee <lechan1317@gmail.com>
2023-11-03 09:33:14 +0900

    Style: formatter of core

--- includes/event/ProgramControl.hpp
+++ includes/event/ProgramControl.hpp
@@ -1,19 +1,18 @@
 #ifndef PROGRAMCONTROL_HPP
 #define PROGRAMCONTROL_HPP
 
-#include "Core.hpp"
 #include "Common.hpp"
+#include "Core.hpp"
 #include "IEventHandler.hpp"
 
 class ProgramControl : public IEventHandler {
 public:
-	virtual void EventHandler(struct kevent &currentEvent);
-	void RegisterTerminationSignals(void);
-	void UnregisterTerminationSignals(void);
+  virtual void EventHandler(struct kevent &currentEvent);
+  void RegisterTerminationSignals(void);
+  void UnregisterTerminationSignals(void);
 
 private:
-	static void signalHandler(int signal);
+  static void signalHandler(int signal);
 };
-
 
 #endif
--- srcs/core/Common.cpp
+++ srcs/core/Common.cpp
@@ -2,5 +2,5 @@
 
 int Common::mKqueue = 0;
 bool Common::mRunning = false;
-Node* Common::mConfigTree = NULL;
-ConfigMap* Common::mConfigMap = NULL;
+Node *Common::mConfigTree = NULL;
+ConfigMap *Common::mConfigMap = NULL;
