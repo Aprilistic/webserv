71d87ca2d130870827d52bbe16810abbf0301828
euiclee <lechan1317@gmail.com>
2023-11-06 09:36:52 +0900

    Test: reverse main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,4 @@
 #include "WebServer.hpp"
-#include "Common.hpp"
-#include "ConfigMap.hpp"
 
 extern char **environ;
 
@@ -13,23 +11,7 @@ int main(int argc, char **argv) {
   } else if (argc == 2) {
 
   WebServer WebServer(argv[1]);
-
-  Node *location = Common::mConfigMap->GetConfigNode(
-      80, "localhost", "/restricted_area.bla");
-  if (location == NULL) {
-    std::cout << "NULL" << std::endl;
-  } else {
-    for (std::map<std::string, std::vector<std::string> >::iterator it =
-            location->mDirectives.begin();
-        it != location->mDirectives.end(); ++it) {
-      std::cout << it->first << " ";
-      for (size_t i = 0; i < it->second.size(); i++) {
-        std::cout << it->second[i] << std::endl;
-      }
-    }
-  }
-
-  // WebServer.Run();
+  WebServer.Run();
   } else {
     std::cout << "Usage: ./" << argv[0] << "[config_file]" << std::endl;
   }
