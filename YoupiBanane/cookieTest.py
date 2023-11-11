b4178b001a7557c904470eb381c256ec0eb716ec
Jinho Heo <uj07077@gmail.com>
2023-11-11 21:18:35 +0900

    Fix: fix http env name in cgi script
    Co-authored-by: taeypark <gitubanana@users.noreply.github.com>

--- YoupiBanane/cookieTest.py
+++ YoupiBanane/cookieTest.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python
-# Python script to show the value of a cookie from an environment variable
 
 import os
 from http.cookies import SimpleCookie
 
-# Assuming the cookie is stored in an environment variable named 'COOKIE_HEADER'
-cookie_header = os.environ.get('COOKIE_HEADER', '')
+cookie_header = os.environ.get('HTTP_COOKIE')
 
-# Check if the cookie_header is not empty
 if cookie_header:
-    # Create a SimpleCookie object and load the cookie header into it
     cookie = SimpleCookie()
     cookie.load(cookie_header)
 
-    # Extract and print the value of a specific cookie, e.g., 'id'
     if 'id' in cookie:
         print("Value of id cookie:", cookie['id'].value)
     else:
